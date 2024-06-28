0c3962625e198e356a7f53180edcbcc65c10535a
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2024-06-28 21:16:59 +0200

    28.06.24

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
 const MaxPokemon = 151;
-const listWrapper = document.getElementById('list-wrapper');
-const searchInput = document.getElementById('search-input');
-const numberFilter = document.getElementById('number');
-const nameFilter = document.getElementById('name');
-const notFoundMessage = document.getElementById('not-found-message');
-const loadingDiv = document.getElementById('loadscreen-div'); // Loading Screen is always here
-
+const listWrapper = document.getElementById("list-wrapper");
+const searchInput = document.getElementById("search-input");
+const numberFilter = document.getElementById("number");
+const nameFilter = document.getElementById("name");
+const notFoundMessage = document.getElementById("not-found-message");
+const loadingDiv = document.getElementById("loadscreen-div"); // Loading Screen is always here
+const loadingSpinner = document.getElementById("loading-spinner");
+let limit = 70;
+let offset = 0;
 let allPokemonsWithId = [];
 
 window.onload = async () => { // Wait for the window to be fully loaded
-    loadingDiv.classList.toggle('d-none'); // Immediately show the loading screen
-    await fetchPokemons(); // Fetch the Pokémon data
-    renderPokemons(allPokemonsWithId); // Render the Pokémon list
-    setTimeout(() => {
-        loadingDiv.classList.toggle('d-none'); // Hide the loading screen after 3 seconds
-    }, 3000); 
-}
+ if (loadingDiv) {
+    loadingDiv.classList.toggle("d-none"); // Immediately show the loading screen
+ }
+ await fetchPokemons(); // Fetch the Pokémon data
+ renderPokemons(allPokemonsWithId); // Render the Pokémon list
+ setTimeout(() => {
+    if (loadingDiv) {
+        loadingDiv.classList.toggle("d-none"); // Hide the loading screen after 3 seconds
+    }
+ }, 3000);
+};
 
 async function fetchPokemons() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-    const data = await response.json();
-    const allPokemons = data.results;
-    allPokemons.forEach((pokemon, index) => {
-        const id = index + 1;
-        allPokemonsWithId.push({ ...pokemon, id });
-    });
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  const data = await response.json();
+  const allPokemons = data.results;
+  allPokemons.forEach((pokemon, index) => {
+    const id = index + 1;
+    allPokemonsWithId.push({ ...pokemon, id });
+  });
 }
 
 function renderPokemons(pokemons) {
-    listWrapper.innerHTML = '';
-    pokemons.forEach((pokemon, index) => {
-        const pokemonId = pokemon.url.split('/')[6]; // Hole die Pokemon-ID aus der URL
-        const listItem = document.createElement("div");
-        listItem.className = "list-item";
-        listItem.innerHTML = getPokemonHtml(pokemonId, capitalizeFirstLetter(pokemon.name));
-        listItem.addEventListener('click', () => window.location.href = `details.html?pokemonId=${pokemonId}`); // Weiterleitung zur Detailseite
-        listWrapper.appendChild(listItem); // Füge das Pokemon zur Liste hinzu
-    });
+  listWrapper.innerHTML = "";
+  pokemons.forEach((pokemon, index) => {
+    const pokemonId = pokemon.url.split("/")[6]; // Hole die Pokemon-ID aus der URL
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = getPokemonHtml(pokemonId,capitalizeFirstLetter(pokemon.name) );
+    listItem.addEventListener("click",() => (window.location.href = `details.html?pokemonId=${pokemonId}`));// Weiterleitung zur Detailseite 
+    listWrapper.appendChild(listItem); // Füge das Pokemon zur Liste hinzu
+  });
 }
 
 function getPokemonHtml(pokemonId, pokemonName) {
-    return `
+  return `
         <div class="number-wrap">
             <p class="caption-fonts">#${pokemonId}</p>
         </div>
@@ -53,44 +59,62 @@ function getPokemonHtml(pokemonId, pokemonName) {
     `;
 }
 
-
-function capitalizeFirstLetter(string) { // This function capitalizes the first letter of a given string
-    return string.charAt(0).toUpperCase() + string.slice(1); // Capitalize the first letter of the string
+function capitalizeFirstLetter(string) {// This function capitalizes the first letter of a given string
+  return string.charAt(0).toUpperCase() + string.slice(1); // Capitalize the first letter of the string
 }
 
-searchInput.addEventListener('input', filterPokemons); // Add the event listener to the input element
+searchInput.addEventListener("input", filterPokemons); // Add the event listener to the input element
 
-function filterPokemons() {   // Filter the Pokémon by the search input
-    const searchValue = searchInput.value.toLowerCase(); //
-    let filteredPokemons;
-    if (numberFilter.checked) {
-        filteredPokemons = allPokemonsWithId.filter(pokemon => // If number filter is checked, perform exact ID match
-            pokemon.id.toString() === searchValue
-        );
-    } else {
-        filteredPokemons = allPokemonsWithId.filter(pokemon => // If name filter is checked, perform name search
-            pokemon.name.startsWith(searchValue)
-        );
-    }
-    renderPokemons(filteredPokemons);
-    if (filteredPokemons.length === 0) {  // If filteredPokemons is empty, show the not found message
-        notFoundMessage.style.display = 'block';   // 
-    } else {
-        notFoundMessage.style.display = 'none'; // If filteredPokemons is not empty, hide the not found message
-    }
+function filterPokemons() {
+  const searchValue = searchInput.value.toLowerCase(); //
+  let filteredPokemons;
+  if (numberFilter.checked) {
+    filteredPokemons = allPokemonsWithId.filter(// If number filter is checked, perform exact ID match
+      (pokemon) => pokemon.id.toString() === searchValue);
+  } else {
+    filteredPokemons = allPokemonsWithId.filter((pokemon) => pokemon.name.startsWith(searchValue)
+    );
+  }
+  renderPokemons(filteredPokemons);
+  if (filteredPokemons.length === 0) {// If filteredPokemons is empty, show the not found message
+     notFoundMessage.style.display = "block"; //
+  } else {
+     notFoundMessage.style.display = "none"; // If filteredPokemons is not empty, hide the not found message
+  }
 }
 
-function sortNumber(){
-    allPokemonsWithId.sort((x,y) =>{
-       const xId =parseInt(x.url.split("/")[6]); // Split the url and get the id
-       const yId =parseInt(y.url.split("/")[6]); // parsing from x, now is Y
-       return xId - yId; // Compare the ids numerically
+function loadMorePokemon() {
+    showLoadingSpinner();
+    offset += limit;
+    fetchPokemons().then(() => {
+      renderPokemons(allPokemonsWithId);
+      hideLoadingSpinner();
     });
-    renderPokemons(allPokemonsWithId);
+  }
+  
+function sortNumber() {
+  allPokemonsWithId.sort((x, y) => {
+    const xId = parseInt(x.url.split("/")[6]); // Split the url and get the id
+    const yId = parseInt(y.url.split("/")[6]); // parsing from x, now is Y
+    return xId - yId; // Compare the ids numerically
+  });
+  renderPokemons(allPokemonsWithId);
 }
 
 async function fetchPokemonById(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemon = await response.json();
-    return pokemon;
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pokemon = await response.json();
+  return pokemon;
+}
+
+function showLoadingSpinner() {
+  if (loadingSpinner) {
+    loadingSpinner.classList.remove("d-none");
+  }
+}
+
+function hideLoadingSpinner() {
+  if (loadingSpinner) {
+    loadingSpinner.classList.add("d-none");
+  }
 }
